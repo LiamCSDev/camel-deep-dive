31179965e242603742a6083859bf969d7a9e8deb
Liam Parsons <liamcsdev@gmail.com>
2023-05-16 16:19:04 +0100

    Create MyFirstTimerRouter.java
    Created my first route with basic functionality

---
+++ camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -0,0 +1,16 @@
+package com.in28minutes.microservices.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstTimerRouter extends RouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+      from("timer:first-timer")
+          .transform().constant("My Constant Message")
+          .to("log:first-timer");
+  }
+
+}
