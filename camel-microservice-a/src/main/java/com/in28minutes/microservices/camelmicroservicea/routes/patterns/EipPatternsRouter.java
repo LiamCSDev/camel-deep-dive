f95bf223d95ae0d8dd827cfc688efcdf7e07f7b3
Liam Parsons <liamcsdev@gmail.com>
2023-05-19 15:19:27 +0100

    Splitter patterns
    Using split() on a csv file

--- camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -9,8 +9,14 @@ public class EipPatternsRouter extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
-    from("timer:multicast?period=10000")
+/*    from("timer:multicast?period=10000")
         .multicast()
         .to("log:something1", "log:something2", "log:something3");
+  */
+
+    from("file:files/csv")
+        .unmarshal().csv()
+        .split(body())
+        .to("log:split-files");
   }
 }
